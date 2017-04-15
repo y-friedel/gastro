48ed84f60e2042f1f5f4f98dcf51ecb82e3c2529
Yoh <friedel.yoann@gmail.Com>
2017-04-15 15:41:05 +0200

    Add Hero Add Scene

---
+++ Assets/scripts/PV.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PV : MonoBehaviour {
+
+    public uint value;
+    public Text linkedText;
+	// Use this for initialization
+	void Start () {
+        value = 100;
+        AddPV(50);
+	}
+
+    public void AddPV(uint pvToAdd)
+    {
+        value += pvToAdd;
+        linkedText.text = value.ToString();
+    }
+
+    public void RemovePV(uint pvToRemove)
+    {
+        value -= pvToRemove;
+        linkedText.text = value.ToString();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
